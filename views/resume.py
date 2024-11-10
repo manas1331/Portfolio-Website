fc163c2eed2fefebeeb07704e9b8b7275d1cea82
Manas R <122677792+manas1331@users.noreply.github.com>
2024-11-10 09:07:16 +0530

    Update resume.py

--- views/resume.py
+++ views/resume.py
@@ -3,14 +3,13 @@ from forms.contact import contact_form
 
 st.title("More About Myself")
 
-
+# Uncomment this function if you want a dialog-based contact form
 # @st.dialog("Contact Me")
 # def show_contact_form():
 #     contact_form()
 
-
 # --- HERO SECTION ---
-col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+col1, col2 = st.columns([1, 3], gap="small")
 with col1:
     st.image("Pictures/Profile_pic.png", width=240)
 
@@ -19,13 +18,12 @@ with col2:
     st.write(
         "|| CSE Pre-Final year Student at PES University | Research Intern at ISFCR(Cybersecurity) | Member at Research Et Al | Samarpana | CMS | Core member at QQC | Kannada Koota ||"
     )
+    # Uncomment the following code to add a Contact Me button
     # if st.button("✉️ Contact Me"):
     #     show_contact_form()
 
-
 # --- EXPERIENCE & QUALIFICATIONS ---
 st.write("\n")
-# st.subheader("Experience & Qualifications", anchor=False)
 st.markdown(
     """
     <h3 style="text-decoration: underline;">Experience & Qualifications</h3>
@@ -51,17 +49,15 @@ st.markdown(
 )
 st.write(
     """
-    - **Developer Tools:** GitHub, Visual studio
+    - **Developer Tools:** GitHub, Visual Studio
     - **Programming:** Python, C++, C, Java, Go, R
     - **Data Visualization:** MS Excel, R
-    - **Databases:**  MongoDB, MySQL
+    - **Databases:** MongoDB, MySQL
     """
 )
 
-
-# --- Positions of Responsblity ---
+# --- POSITIONS OF RESPONSIBILITY ---
 st.write("\n")
-# st.subheader("Positions of Responsblity", anchor=False)
 st.markdown(
     """
     <h3 style="text-decoration: underline;">Positions of Responsibility</h3>
@@ -71,18 +67,17 @@ st.markdown(
 st.write(
     """
     - Design Team (Core), QQC club (National level Conquiztador Quiz)
-    - Design Team, Research Et AL club (Inquisto,Articulate,Erudio podcast)
-    - Design Team(Core), Kannada Koota club(Kannada Rajyotsva-2023)
+    - Design Team, Research Et AL club (Inquisto, Articulate, Erudio podcast)
+    - Design Team (Core), Kannada Koota club (Kannada Rajyotsva-2023)
     - Logistics Team, Samarpana club (Samarpana Run-2023)
-    - Operations Team, CMS Club(Green Carnival)
+    - Operations Team, CMS Club (Green Carnival)
     """
 )
 
+# --- RESUME LINK ---
 with st.expander("##### **Resume Link**"):
     st.write("Click the link below to view my resume:")
-   # Define the HTML for the styled box
-    st.markdown('<a href="https://drive.google.com/file/d/1g6QPJrjUPHyAQx9TojSrXPP-0FT_geHA/view?usp=drive_link" target="_blank">**View Resume**</a>', unsafe_allow_html=True) 
-
-
-
-
+    st.markdown(
+        '<a href="https://drive.google.com/file/d/1g6QPJrjUPHyAQx9TojSrXPP-0FT_geHA/view?usp=drive_link" target="_blank">**View Resume**</a>',
+        unsafe_allow_html=True
+    )
